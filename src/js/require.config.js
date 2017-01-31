010858aa37223f993ecfc21c6ae44f5bdfeeabfd
mjason <mjs1125@naver.com>
2017-01-31 10:01:20 +0900

    overwatch project 2017-01-31

---
+++ src/js/require.config.js
@@ -0,0 +1,22 @@
+require.config({
+	baseUrl: "js",
+
+	paths: {
+		"async"         : "vendor/requirejs/async",
+		"console"       : "plugins",
+		"jquery"        : "vendor/jquery-{{JQUERY_VERSION}}.min",
+		"bootstrap"     : "vendor/bootstrap/js/bootstrap.min",
+		"clipboard"     : "vendor/clipboard/clipboard.min",
+
+		"common"        : "./common",
+		"easteregg"     : "easteregg"
+	},
+
+	shim: {
+		"bootstrap": {
+			deps: ["jquery"]
+		}
+	},
+
+	deps: ["console"]
+});
